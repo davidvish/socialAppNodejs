b4b40cf76df3e0563d8531afdd7a7e0660e121ce
davidvishr <davidvishwakarma6991@gmail.com>
2024-01-14 15:59:47 +0530

    change

--- index.js
+++ index.js
@@ -30,27 +30,15 @@ app.use("/social/api/post",postRouter)
 app.use("/social/api/post/comment",commentRouter)
 
 
-const PORT = process.env.PORT || 3000
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
-//Routes go here
-app.all('*', (req,res) => {
-  res.json({"every thing":"is awesome"})
-})
-
-//Connect to the database before listening
-connectDB().then(() => {
-  app.listen(PORT, () => {
-      console.log("listening for requests");
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-})
-
-
+  .then(() => {
+    console.log("Mogodb database is connet");
+  });
+app.listen(8200, () => {
+  console.log("node connection checking check port " + 8200);
+});
